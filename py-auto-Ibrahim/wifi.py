57f2ee2f80a4e9feae1207759375854d2c11faed
Ibrahim Abou Elenein <ibrahimabouelanin@gmail.com>
2021-05-02 04:05:30 +0200

    upload

--- py-auto-Ibrahim/wifi.py
+++ py-auto-Ibrahim/wifi.py
@@ -1,10 +1,28 @@
-#!/usr/bin/python3 
-
+#!/usr/bin/env python3
 import subprocess
-import os
-from itertools import product,permutations
-from string import digits, ascii_letters
+import sys
 
-for i in product(ascii_letters,repeat=4):
-  print(''.join(i))
+
+def run_command(command):
+    output,_ = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, shell=True).communicate()
+    return output.decode('utf-8').rstrip('\r\n')
+
+def get_ssid():    
+    try:
+        ssid = run_command(
+            "nmcli -t -f active,ssid dev wifi | grep '^yes:' | sed 's/^yes://'"
+        )
+    except:
+        print("Error")
+        sys.exit(1)    
+    return ssid    
+def get_password(ssid):
+    get_password_command = f'nmcli -s -g 802-11-wireless-security.psk connection show "{ssid}"'
+    password = run_command(f'sudo {get_password_command}')
+    return password
+if __name__ == "__main__":
+    ssid = get_ssid()
+    password = get_password(ssid)
+    print(password)
 
